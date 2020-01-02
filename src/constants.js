149a6224203c06a779c41127f52e7a3f1f92a8ff
Tommy Long Pham <tpham@smugmug.com>
2020-01-02 13:41:50 -0800

    tokenize

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,5 @@ export const PARENTHESIS = "Parenthesis";
 export const NUMBER = "Number";
 export const NAME = "Name";
 export const STRING = "String";
+export const NUMBERIC_LITERAL = "NumericLiteral";
+export const CALL_EXPRESSION = "CallExpression";
