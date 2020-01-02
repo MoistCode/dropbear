0034b12e91f8dc1d1597e292f34fd39790e1f2c3
Tommy Long Pham <tpham@smugmug.com>
2020-01-02 13:52:33 -0800

    tokenize

--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,5 @@ export const NAME = "Name";
 export const STRING = "String";
 export const NUMBERIC_LITERAL = "NumericLiteral";
 export const CALL_EXPRESSION = "CallExpression";
+export const STRING_LITERAL = "StringLiteral";
+export const IDENTIFIER = "Identifier";
--- src/parse.js
+++ src/parse.js
@@ -1,18 +1,38 @@
 const { isOpeningParenthesis, isClosingParenthesis } = require('./identify');
 const { specialForms } = require('./special-forms');
 const { peek, pop } = require('./utilities');
-const { NUMBER, NUMBERIC_LITERAL } = require('./constants');
+const {
+  NUMBER,
+  STRING,
+  NAME,
+  NUMBERIC_LITERAL,
+  STRING_LITERAL,
+  IDENTIFIER,
+} = require('./constants');
 
-const parenthesize = () => {};
+const parenthesize = tokens => {
+  return tokens;
+};
 
-const parse = (token) => {
+const parse = tokens => {
   const token = pop(tokens);
 
-  if (token.type === NUMBER) {
-    return {
-      type: NUMBERIC_LITERAL,
-      value: token.value,
-    };
+  switch (token.type) {
+    case NUMBER:
+      return {
+        type: NUMBERIC_LITERAL,
+        value: token.value,
+      };
+    case STRING:
+      return {
+        type: STRING_LITERAL,
+        value: token.value,
+      };
+    case NAME:
+      return {
+        type: IDENTIFIER,
+        name: token.value,
+      };
   }
 };
 
