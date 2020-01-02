f9c463d930455025eb13ad9cffd600fcaa3ccad6
Tommy Long Pham <tpham@smugmug.com>
2020-01-02 14:46:26 -0800

    evaluate

--- src/evaluate.js
+++ src/evaluate.js
@@ -1,6 +1,24 @@
 const { environment } = require('./standard-library');
+const { CALL_EXPRESSION } = require('./constants');
 const last = collection => collection[collection.length - 1];
 
-const evaluate = () => {};
+const apply = node => {
+  const fn = environment[node.name];
+  const args = node.arguments.map(evaluate);
+
+  if (typeof fn !== 'function') {
+    throw new TypeError(`${node.name} is not a function`);
+  }
+  return fn(...args);
+}
+
+const evaluate = (node) => {
+  switch (node.type) {
+    case CALL_EXPRESSION:
+      return apply(node);
+  }
+
+  if (node.value) return node.value;
+};
 
 module.exports = { evaluate };
