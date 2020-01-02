2c80d6ff6b5918ba67bf4520ff216e71399310f4
Tommy Long Pham <tpham@smugmug.com>
2020-01-02 13:45:45 -0800

    parser

--- src/parse.js
+++ src/parse.js
@@ -1,9 +1,19 @@
 const { isOpeningParenthesis, isClosingParenthesis } = require('./identify');
 const { specialForms } = require('./special-forms');
 const { peek, pop } = require('./utilities');
+const { NUMBER, NUMBERIC_LITERAL } = require('./constants');
 
 const parenthesize = () => {};
 
-const parse = () => {};
+const parse = (token) => {
+  const token = pop(tokens);
+
+  if (token.type === NUMBER) {
+    return {
+      type: NUMBERIC_LITERAL,
+      value: token.value,
+    };
+  }
+};
 
 module.exports = { parse: tokens => parse(parenthesize(tokens)) };
