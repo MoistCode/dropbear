0ded013b7087f49e41af56ee04246eec1dc78e26
Tommy Long Pham <tpham@smugmug.com>
2020-01-02 13:00:59 -0800

    tokenizer

--- src/tokenize.js
+++ src/tokenize.js
@@ -7,12 +7,18 @@ const {
 } = require('./identify');
 
 const tokenize = (input) => {
-  let cursor = 0;
   const tokens = [];
+  let cursor = 0;
+
+  // for (; cursor < input.length; cursor++) {
+
+  // }
 
   while (cursor < input.length) {
-    
+    cursor ++;
   }
+
+  return tokens;
 };
 
 module.exports = { tokenize };
